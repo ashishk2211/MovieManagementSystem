bdc1dfafc4a9d7ce282fb9f49da9ba4a2cb2b35d
shubhamsingh0523 <44120076+shubhamsingh0523@users.noreply.github.com>
2020-09-19 20:29:12 +0530

    Add files via upload

---
+++ MovieManagement/src/main/java/com/cg/movieManagement/MovieManagementApplication.java
@@ -0,0 +1,12 @@
+package com.cg.movieManagement;
+
+import org.springframework.boot.SpringApplication;
+
+public class MovieManagementApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MovieManagementApplication.class, args);
+	}
+
+}

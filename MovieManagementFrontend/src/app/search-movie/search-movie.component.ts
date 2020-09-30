1f45aa5c96ede805f1a3b1ddab112c8957dc0834
ashishk2211 <ashish2211761@live.com>
2020-09-30 17:43:52 +0530

    frontend completed

---
+++ MovieManagementFrontend/src/app/search-movie/search-movie.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MovieService } from '../movie.service';
+import { combineLatest } from 'rxjs';
+
+
+@Component({
+  selector: 'app-search-movie',
+  templateUrl: './search-movie.component.html',
+  styleUrls: ['./search-movie.component.css']
+})
+export class SearchMovieComponent implements OnInit {
+
+
+  movieList:any ;
+  name:string;
+  toggleMovieList:boolean =false;
+
+  errorMsg: string;
+  showError:boolean;
+
+  constructor(private movieService :MovieService) {
+  }
+
+  ngOnInit() {
+ }
+
+  searchMovie(form: NgForm){
+    this.showError=false;
+    this.toggleMovieList=false;
+    
+    this.movieService.searchMovie(this.name).subscribe(
+     data=>{this.movieList=data,this.toggleMovieList=true},
+     error=>{this.errorMsg=error.error.message, this.showError=true}
+    );
+     form.resetForm();
+  }
+
+}
